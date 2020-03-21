c7513ccfe78084e7f8cf20126b248d8106471e82
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-21 21:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'ejs') 
   .get('/', (req, res) => res.sendFile(__dirname + "/view.html"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
  
